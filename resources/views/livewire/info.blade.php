e073ef627f5ba11341e44e5635f08b7c53d0d840
Miguel Navas <miguelnavas1991@gmail.com>
2025-08-18 17:15:51 -0400

    color 0

--- resources/views/livewire/info.blade.php
+++ resources/views/livewire/info.blade.php
@@ -399,7 +399,7 @@
                                 <x-label>Color</x-label>
                                 <select wire:model='color'
                                     class="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                    <option value="">Seleccione..</option>
+                                    <option value="0">Seleccione..</option>
                                     @if ($colors)
                                         @foreach ($colors as $color)
                                             <option value="{{ $color->id }}">{{ $color->nombre }}</option>

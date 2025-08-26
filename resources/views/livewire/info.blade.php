bc070c0845e4bc193114b0094236cc84fb719f78
Miguel Navas <miguelnavas1991@gmail.com>
2025-08-26 17:53:20 -0400

    validando campos

--- resources/views/livewire/info.blade.php
+++ resources/views/livewire/info.blade.php
@@ -163,7 +163,7 @@
                                 <x-label>Tipo</x-label>
                                 <select wire:model='tipo'
                                     class="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                    <option value="">Seleccione..</option>
+                                    <option value="0">Seleccione..</option>
 
                                     @if ($medidas)
                                         @foreach ($medidas as $medida)
@@ -502,7 +502,7 @@
                             <x-label>Tipo</x-label>
                             <select wire:model='tipo'
                                 class="block w-full rounded-md border-0 py-1 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6">
-                                <option value="">Seleccione..</option>
+                                <option value="0">Seleccione..</option>
 
                                 @if ($medidas)
                                     @foreach ($medidas as $medida)
